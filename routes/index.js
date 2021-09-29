d5f4ba6501593ab62ca6ae6d2fb1c0f050e0fc4d
Sammmmmuel <69543188+Sammmmmuel@users.noreply.github.com>
2021-09-29 10:50:26 -0700

    Users can search pets by name and breed

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,10 @@ const Pet = require('../models/pet');
 
 module.exports = (app) => {
 
-  /* GET home page. */
-  app.get('/', (req, res) => {
-    Pet.find().exec((err, pets) => {
-      res.render('pets-index', { pets: pets });    
+    /* GET home page. */
+    app.get('/', (req, res) => {
+        Pet.find().exec((err, pets) => {
+            res.render('pets-index', { pets: pets });
+        });
     });
-  });
 }
